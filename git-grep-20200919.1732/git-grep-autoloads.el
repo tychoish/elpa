8b67aa8e085c250f7649210ab5244635f5ec13ab
tycho garen <garen@tychoish.com>
2020-09-19 21:38:19 -0400

    update

---
+++ git-grep-20200919.1732/git-grep-autoloads.el
@@ -0,0 +1,22 @@
+;;; git-grep-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "git-grep" "git-grep.el" (0 0 0 0))
+;;; Generated autoloads from git-grep.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "git-grep" '("git-grep")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; git-grep-autoloads.el ends here
