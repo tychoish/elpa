7e8a0336106c17f30761a82af7e41e6f5ddced24
tycho garen <garen@tychoish.com>
2022-11-27 20:20:36 -0500

    more cleanup

--- autothemer-20220829.305/autothemer-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from autothemer.el  -*- no-byte-compile: t -*-
-(define-package "autothemer" "20220829.305" "Conveniently define themes" '((dash "2.10.0") (emacs "26.1")) :commit "d47f7b5ba7caef72dc2ea8f8f3cf9b3e30c63b10" :maintainer '("Jason Milkins" . "jasonm23@gmail.com") :url "https://github.com/jasonm23/autothemer")
--- base16-theme-20220828.153/base16-theme-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "base16-theme" "20220828.153" "Collection of themes built on combinations of 16 base colors" 'nil :commit "a53091ca225b70136e8e3a1f693d0e856022dd31" :authors
-  '(("Kaleb Elwert" . "belak@coded.io")
-    ("Neil Bhakta"))
-  :maintainer
-  '("Kaleb Elwert" . "belak@coded.io")
-  :url "https://github.com/base16-project/base16-emacs")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
