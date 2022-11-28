873771d0f35d814bf6a377b4e41f9f2cd1f1e2f7
tycho garen <garen@tychoish.com>
2022-11-28 09:52:46 -0500

    updates

--- lsp-ui-20220924.850/lsp-ui-pkg.el
+++ lsp-ui-20220924.850/lsp-ui-pkg.el
@@ -1,17 +1,9 @@
-<<<<<<<< HEAD:lsp-ui-20220823.1626/lsp-ui-pkg.el
-(define-package "lsp-ui" "20220823.1626" "UI modules for lsp-mode"
-========
 (define-package "lsp-ui" "20220924.850" "UI modules for lsp-mode"
->>>>>>>> c7fcdd5f (update lsp):lsp-ui-20220924.850/lsp-ui-pkg.el
   '((emacs "26.1")
     (dash "2.18.0")
     (lsp-mode "6.0")
     (markdown-mode "2.3"))
-<<<<<<<< HEAD:lsp-ui-20220823.1626/lsp-ui-pkg.el
-  :commit "d8cce7dc154aa3216c080dd5c6fb827bdba9a312" :authors
-========
   :commit "3cd7cc61273341023b863dcf45906ac9142fd1aa" :authors
->>>>>>>> c7fcdd5f (update lsp):lsp-ui-20220924.850/lsp-ui-pkg.el
   '(("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me"))
   :maintainer
   '("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me")
