d0cf11adbbad4cd6bf5736532129185db160b299
tycho garen <garen@tychoish.com>
2023-10-29 19:53:03 -0400

    clean up cruft

--- thingopt-20160520.2318/thingopt-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "thingopt" "20160520.2318" "Thing at Point optional utilities" 'nil :commit "5679815852652479f3b3c9f3a98affc927384b2c" :keywords '("convenience") :authors '(("Tomohiro Matsuyama" . "m2ym.pub@gmail.com")) :maintainer '("Tomohiro Matsuyama" . "m2ym.pub@gmail.com"))
