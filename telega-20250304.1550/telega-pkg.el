7764827152502bf94edaa8d19fb498fbfb4009f7
tycho garen <garen@tychoish.com>
2025-06-02 18:21:12 -0400

    upgrade

---
+++ telega-20250304.1550/telega-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "telega" "20250304.1550"
+  "Telegram client (unofficial)."
+  '((emacs              "27.1")
+    (visual-fill-column "1.9")
+    (transient          "0.3.0"))
+  :url "https://github.com/zevlg/telega.el"
+  :commit "ff06f58364375c96477561f265e3dbf55a8ad231"
+  :revdesc "ff06f5836437"
+  :keywords '("comm")
+  :authors '(("Zajcev Evgeny" . "zevlg@yandex.ru"))
+  :maintainers '(("Zajcev Evgeny" . "zevlg@yandex.ru")))
