1720ef3cdc3b3dc00286e4a4aac06a483cf10f36
tycho <garen@tychoish.com>
2025-08-26 22:32:07 -0400

    cleanup unused packages and add yaml-pro

---
+++ yaml-pro-20250817.452/yaml-pro-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "yaml-pro" "20250817.452"
+  "Parser-aided YAML editing features."
+  '((emacs "26.1")
+    (yaml  "0.5.1"))
+  :url "https://github.com/zkry/yaml-pro"
+  :commit "9b9509188e5b88bb933e98ab36ab992519b9554b"
+  :revdesc "9b9509188e5b"
+  :keywords '("tools"))
