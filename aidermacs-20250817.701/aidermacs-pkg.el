c3b4e9eea81a5b097f1f63872e137592beb371e5
tycho <garen@tychoish.com>
2025-09-02 12:23:24 -0400

    add aidermacs

---
+++ aidermacs-20250817.701/aidermacs-pkg.el
@@ -0,0 +1,13 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "aidermacs" "20250817.701"
+  "AI pair programming with Aider."
+  '((emacs         "26.1")
+    (transient     "0.3.0")
+    (compat        "30.0.2.0")
+    (markdown-mode "2.7"))
+  :url "https://github.com/MatthewZMD/aidermacs"
+  :commit "cdee470ee5aa5c81364799fa46f9dbd5ef588663"
+  :revdesc "cdee470ee5aa"
+  :keywords '("ai" "emacs" "llm" "aider" "ai-pair-programming" "tools")
+  :authors '(("Mingde Zeng" . "matthewzmd@posteo.net"))
+  :maintainers '(("Mingde Zeng" . "matthewzmd@posteo.net")))
