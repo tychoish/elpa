89fccf6ff3b37c604c9a518b34669be79ca7772b
tycho <garen@tychoish.com>
2025-09-02 16:54:12 -0400

    pkg: add adder back

---
+++ aider-20250826.400/aider-pkg.el
@@ -0,0 +1,14 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "aider" "20250826.400"
+  "AI assisted programming with Aider and LLM."
+  '((emacs         "26.1")
+    (transient     "0.9.0")
+    (magit         "2.1.0")
+    (markdown-mode "2.5")
+    (s             "1.13.0"))
+  :url "https://github.com/tninja/aider.el"
+  :commit "f7d467fe6497d8cd8a3b4267f5f4bc93e5c669ef"
+  :revdesc "f7d467fe6497"
+  :keywords '("ai" "gpt" "sonnet" "llm" "aider" "gemini-pro" "deepseek" "ai-assisted-coding")
+  :authors '(("Kang Tu" . "tninja@gmail.com"))
+  :maintainers '(("Kang Tu" . "tninja@gmail.com")))
