7ae0d8c2612c10e00acde8b0c23d024fabe2c3ea
tycho garen <garen@tychoish.com>
2026-05-12 07:52:41 -0400

    feat: magit-gh

---
+++ magit-gh-20260221.522/magit-gh-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "magit-gh" "20260221.522"
+  "GitHub CLI integration for Magit."
+  '((emacs     "29.1")
+    (magit     "4.0.0")
+    (transient "0.5.0"))
+  :url "https://github.com/jonathanchu/magit-gh"
+  :commit "5b3cb8f367e3d9fbb5d9593683b492bd7b5399c7"
+  :revdesc "5b3cb8f367e3"
+  :keywords '("git" "tools" "vc" "github")
+  :authors '(("Jonathan Chu" . "me@jonathanchu.is"))
+  :maintainers '(("Jonathan Chu" . "me@jonathanchu.is")))
