b640272f5a825515d876faa682ef6d2b51cde246
tycho <garen@tychoish.com>
2026-08-24 01:40:33 -0400

    chore: bump package lint

---
+++ package-lint-20260808.1622/package-lint-pkg.el
@@ -0,0 +1,13 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "package-lint" "20260808.1622"
+  "A linting library for elisp package authors."
+  '((emacs     "25.1")
+    (let-alist "1.0.6"))
+  :url "https://github.com/purcell/package-lint"
+  :commit "ade52be18035b1e1dbc5b65f2100be59e78e76e1"
+  :revdesc "ade52be18035"
+  :keywords '("lisp")
+  :authors '(("Steve Purcell" . "steve@sanityinc.com")
+             ("Fanael Linithien" . "fanael4@gmail.com"))
+  :maintainers '(("Steve Purcell" . "steve@sanityinc.com")
+                 ("Fanael Linithien" . "fanael4@gmail.com")))
