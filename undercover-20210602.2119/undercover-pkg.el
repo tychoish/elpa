f4579c47a8f72620d5ac181b0f1e78115732ab62
tycho garen <garen@tychoish.com>
2026-08-31 22:35:02 -0400

    feat: coverage packages

---
+++ undercover-20210602.2119/undercover-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "undercover" "20210602.2119"
+  "Test coverage library for Emacs Lisp."
+  '((emacs   "24")
+    (dash    "2.0.0")
+    (shut-up "0.3.2"))
+  :url "https://github.com/sviridov/undercover.el"
+  :commit "1d3587f1fad66a747688f36636b67b33b73447d3"
+  :revdesc "1d3587f1fad6"
+  :keywords '("lisp" "tests" "coverage" "tools")
+  :authors '(("Sviridov Alexander" . "sviridov.vmi@gmail.com"))
+  :maintainers '(("Sviridov Alexander" . "sviridov.vmi@gmail.com")))
