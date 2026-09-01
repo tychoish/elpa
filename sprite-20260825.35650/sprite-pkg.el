9f16358e97b4418c3efed0530a7cae52c8a21ed4
tycho <garen@tychoish.com>
2026-09-01 10:59:21 -0400

    chore: sprite cleanup

--- sprite-20260825.35650/sprite-pkg.el
+++
@@ -1,4 +0,0 @@
-;; -*- no-byte-compile: t -*-
-(define-package "sprite" "20260825.35650" "Manage subordinate Emacs daemon instances"
-  '((emacs "29.1") (seq "2.24"))
-  :url "https://github.com/tychoish/sprite" :keywords "tools" "daemon" "processes")
